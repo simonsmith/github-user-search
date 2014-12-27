0a2f7ae717300670c37f5de934cba659382f2a88
Simon Smith <me@simonsmith.io>
2014-12-27 11:27:20 +0000

    Add popular repos data store

--- app/react/stores/repos.js
+++ app/react/stores/repos.js
@@ -8,12 +8,40 @@ module.exports = Reflux.createStore({
     this.listenTo(UserActions.userRepos, this.onUserRepos);
   },
 
+  removeForks: function(item) {
+    return item.fork == false;
+  },
+
+  sortByPopular: function(a, b) {
+    return b.stargazers_count - a.stargazers_count;
+  },
+
+  trimObjectKeys: function(repo) {
+    return pick(repo,
+      'id',
+      'name',
+      'html_url',
+      'description',
+      'stargazers_count',
+      'watchers_count',
+      'language',
+      'forks_count',
+      'homepage'
+    )
+  },
+
   onUserRepos: function(username) {
     req({
       url: 'https://api.github.com/users/{username}/repos'.replace('{username}', username),
       type: 'json'
     })
       .then(function(data) {
+        data = data
+          .filter(this.removeForks)
+          .sort(this.sortByPopular)
+          .map(this.trimObjectKeys)
+          .slice(0, 5);
+
         this.trigger({
           repos: data
         });
