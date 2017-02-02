c6160e20a144d849caff18ab0f2fc0f2c1c0aec8
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-02 21:10:56 +0000

    Wrap search tests in a describe

--- test/actions/search.spec.js
+++ test/actions/search.spec.js
@@ -1,38 +1,42 @@
 import * as actions from '../../src/actions/search';
 
-describe('when requesting users via a search term', () => {
-  it('should dispatch a SEARCH_REQUEST action', () => {
-    expect(
-      actions.searchRequest({searchTerm: 'simon'})
-    ).toMatchSnapshot();
-  });
-});
+describe('Actions: search', () => {
 
-describe('when receiving a set of users', () => {
-  it('should dispatch a SEARCH_SUCCESS action with the necessary data', () => {
-    const data = {
-      entities: {
-        users: {
-          123: {login: 'foo', id: 123, other: 'test'},
-          456: {login: 'baz', id: 456, other: 'test'},
+  describe('when requesting users via a search term', () => {
+    it('should dispatch a SEARCH_REQUEST action', () => {
+      expect(
+        actions.searchRequest({searchTerm: 'simon'})
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('when receiving a set of users', () => {
+    it('should dispatch a SEARCH_SUCCESS action with the necessary data', () => {
+      const data = {
+        entities: {
+          users: {
+            123: {login: 'foo', id: 123, other: 'test'},
+            456: {login: 'baz', id: 456, other: 'test'},
+          },
         },
-      },
-      result: [123, 456],
-    };
-    expect(
-      actions.searchSuccess(data)
-    ).toMatchSnapshot();
+        result: [123, 456],
+      };
+      expect(
+        actions.searchSuccess(data)
+      ).toMatchSnapshot();
+    });
   });
-});
 
-describe('when an error occurs during a request', () => {
-  it('should dispatch a SEARCH_FAILURE action', () => {
-    const error = new Error('something broke');
-    error.response = {
-      statusText: '404',
-    };
-    expect(
-      actions.searchFailure(error)
-    ).toMatchSnapshot();
+  describe('when an error occurs during a request', () => {
+    it('should dispatch a SEARCH_FAILURE action', () => {
+      const error = new Error('something broke');
+      error.response = {
+        statusText: '404',
+      };
+      expect(
+        actions.searchFailure(error)
+      ).toMatchSnapshot();
+    });
   });
+
 });
