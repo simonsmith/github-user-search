89128b16a72868356d3dc2998755b0d36ac00596
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-09 00:02:37 +0000

    Replace github-api lib with custom version

--- src/store/api.js
+++ src/store/api.js
@@ -1,7 +1,33 @@
-import GitHub from 'github-api';
+import axios from 'axios';
+import assignAll from 'lodash/fp/assignAll';
+import parseLinkHeader from 'parse-link-header';
+import pick from 'lodash/fp/pick';
 
-const api = new GitHub({
-  token: process.env.USER_SEARCH_OAUTH,
+const github = axios.create({
+  baseURL: 'https://api.github.com/',
 });
 
-export default api;
+const token = process.env.USER_SEARCH_OAUTH;
+if (token) {
+  github.defaults.headers.common.Authorization = `token ${token}`;
+}
+
+function transformResponse(response) {
+  const {data} = response;
+  const pagination = parseLinkHeader(response.headers.link);
+  return assignAll([
+    pick(['total_count', 'items'], data),
+    {pagination},
+  ]);
+}
+github.interceptors.response.use(transformResponse);
+
+function searchUsers(params: Object): Promise {
+  return github.get('/search/users', {
+    params,
+  });
+}
+
+export default {
+  searchUsers,
+};
