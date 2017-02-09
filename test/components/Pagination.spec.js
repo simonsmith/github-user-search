81b7cb89a55b4cf6ec79d144f308ed39cfa63108
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-09 20:12:03 +0000

    Add Pagination tests

---
+++ test/components/Pagination.spec.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
+import Pagination from '../../src/components/Pagination';
+
+function createComponent(props) {
+  return renderer.create(
+    <Router>
+      <Pagination {...props} />
+    </Router>
+  );
+}
+
+describe('Component: Pagination', () => {
+
+  describe('when passed a next prop', () => {
+    it('should render a next link', () => {
+      const props = {
+        next: {
+          url: 'https://api.github.com/search/users?q=simon&page=2',
+        },
+      };
+      const component = createComponent(props);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('when passed a prev prop', () => {
+    it('should render a previous link', () => {
+      const props = {
+        prev: {
+          url: 'https://api.github.com/search/users?q=simon&page=1',
+        },
+      };
+      const component = createComponent(props);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('when passed a prev and next prop', () => {
+    it('should render a previous and next link', () => {
+      const props = {
+        prev: {
+          url: 'https://api.github.com/search/users?q=simon&page=1',
+        },
+        next: {
+          url: 'https://api.github.com/search/users?q=simon&page=2',
+        },
+      };
+      const component = createComponent(props);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+});
