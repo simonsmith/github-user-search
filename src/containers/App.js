af9c484dddaffc106208181c93d9611b5a611f1b
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-12 18:22:18 +0000

    Remove styles from App

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,10 +7,6 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
-import {
-  StyleSheet,
-  css,
-} from 'aphrodite/no-important';
 import 'suitcss-base';
 import SearchScreen from '../screens/SearchScreen';
 import Profile from '../screens/Profile';
@@ -18,20 +14,11 @@ import Profile from '../screens/Profile';
 export default function App() {
   return (
     <Router>
-      <div className={css(styles.App)}>
-        <Switch>
-          <Route exact={true} path="/search" component={SearchScreen} />
-          <Route exact={true} path="/:username" component={Profile} />
-          <Redirect to="/search" />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact={true} path="/search" component={SearchScreen} />
+        <Route exact={true} path="/:username" component={Profile} />
+        <Redirect to="/search" />
+      </Switch>
     </Router>
   );
 }
-
-const styles = StyleSheet.create({
-  App: {
-    padding: 15,
-    maxWidth: 1060,
-  },
-});
