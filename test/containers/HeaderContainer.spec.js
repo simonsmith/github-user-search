68127654ffb4cd2e7f783790c802e69968aeabcc
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-20 16:26:15 +0000

    Add tests for HeaderContainer

---
+++ test/containers/HeaderContainer.spec.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {StyleSheetTestUtils} from 'aphrodite';
+
+import {HeaderContainer} from 'containers/Header';
+
+describe('Component: HeaderContainer', () => {
+
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  describe('when the searchQuery prop is not present', () => {
+    it('should not call the search action', () => {
+      const spy = jest.fn();
+      shallow(
+        <HeaderContainer
+          searchQuery={''}
+          searchUser={spy}
+          onSubmit={jest.fn()}
+        />
+      );
+      expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when the searchQuery prop is present', () => {
+    it('should call the search action', () => {
+      const spy = jest.fn();
+      shallow(
+        <HeaderContainer
+          searchQuery={'?q=test'}
+          searchUser={spy}
+          onSubmit={jest.fn()}
+        />
+      );
+      expect(spy.mock.calls[0]).toMatchSnapshot();
+    });
+  });
+
+  describe('when the searchQuery prop changes', () => {
+    it('should call the search action with the new value', () => {
+      const spy = jest.fn();
+      const wrapper = shallow(
+        <HeaderContainer
+          searchQuery={'?q=test'}
+          searchUser={spy}
+          onSubmit={jest.fn()}
+        />
+      );
+      expect(spy.mock.calls[0]).toMatchSnapshot();
+      wrapper.setProps({searchQuery: '?q=foobar'});
+      expect(spy.mock.calls[1]).toMatchSnapshot();
+    });
+  });
+
+});
--- test/containers/SearchResultsContainer.spec.js
+++ test/containers/SearchResultsContainer.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
 import {StyleSheetTestUtils} from 'aphrodite';
 
@@ -13,57 +12,6 @@ describe('Component: SearchResultsContainer', () => {
 
   afterAll(() => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
-
-  describe.skip('when the search prop is not present', () => {
-    it('should not call the search action', () => {
-      const spy = jest.fn();
-      shallow(
-        <SearchResultsContainer
-          searchTerm={''}
-          userEntities={{}}
-          userIds={[]}
-          totalResults={0}
-        />
-      );
-      expect(spy).not.toHaveBeenCalled();
-    });
-  });
-
-  describe.skip('when the search prop is present', () => {
-    it('should call the search action', () => {
-      const spy = jest.fn();
-      shallow(
-        <SearchResultsContainer
-          searchQuery={'?q=test'}
-          searchUser={spy}
-          onSubmit={jest.fn()}
-          userEntities={{}}
-          userIds={[]}
-          totalResults={0}
-        />
-      );
-      expect(spy.mock.calls[0]).toMatchSnapshot();
-    });
-  });
-
-  describe.skip('when the search prop changes', () => {
-    it('should call the search action with the new value', () => {
-      const spy = jest.fn();
-      const wrapper = shallow(
-        <SearchResultsContainer
-          searchQuery={'?q=test'}
-          searchUser={spy}
-          onSubmit={jest.fn()}
-          userEntities={{}}
-          userIds={[]}
-          totalResults={0}
-        />
-      );
-      expect(spy.mock.calls[0]).toMatchSnapshot();
-      wrapper.setProps({search: '?q=foobar'});
-      expect(spy.mock.calls[1]).toMatchSnapshot();
-    });
   });
 
   describe('when the searchTerm is falsey', () => {
