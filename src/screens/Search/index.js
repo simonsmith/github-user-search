a7671b25a649aaaca17501a9e72b7b68e4a8d30d
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-21 15:16:50 +0000

    Add HOC for base screen layout

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,70 +1,13 @@
 // @flow
 
-import React, {
-  PropTypes,
-} from 'react';
-import qs from 'query-string';
-import DocumentTitle from 'react-document-title';
-import isEmpty from 'lodash/fp/isEmpty';
-import curry from 'lodash/fp/curry';
-import {
-  css,
-  StyleSheet,
-} from 'aphrodite/no-important';
+import React from 'react';
 import SearchResultsContainer from 'containers/SearchResults';
-import HeaderContainer from 'containers/Header';
-import {PER_PAGE} from 'store/api';
+import withBaseScreen from 'hoc/withBaseScreen';
 
-type Props = {
-  push: Function,
-  location: Object,
-};
-
-export const pushUrlQuery = curry((push: Function, query: string) => {
-  push({
-    path: '/',
-    search: `per_page=${PER_PAGE}&page=1&q=${query}`,
-  });
-});
-
-export function constructTitle(search: Object) {
-  const base = 'Github User Search';
-  if (isEmpty(search)) {return base;}
-  const {q, page} = search;
-  return `${q} - Page ${page} - ${base}`;
-}
-
-function SearchScreen({location, push}: Props) {
-  const parsedSearch = qs.parse(location.search);
-  const title = constructTitle(parsedSearch);
-
+function SearchScreen(props: Object) {
   return (
-    <DocumentTitle title={title}>
-      <div className={css(styles.SearchScreen)}>
-        <HeaderContainer
-          searchTerm={parsedSearch.q}
-          searchQuery={qs.stringify(parsedSearch)}
-          onSubmit={pushUrlQuery(push)}
-        />
-        <SearchResultsContainer
-          searchTerm={parsedSearch.q}
-        />
-      </div>
-    </DocumentTitle>
+    <SearchResultsContainer {...props} />
   );
 }
 
-SearchScreen.propTypes = {
-  push: PropTypes.func.isRequired,
-  location: PropTypes.object.isRequired,
-};
-
-const styles = StyleSheet.create({
-  SearchScreen: {
-    flexDirection: 'column',
-    display: 'flex',
-    minHeight: '100vh',
-  },
-});
-
-export default SearchScreen;
+export default withBaseScreen(SearchScreen);
