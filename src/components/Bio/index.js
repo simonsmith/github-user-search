f0d9593489e54c68881db7664e3ce6f74fc0f2af
Simon Smith <simonsmith@users.noreply.github.com>
2017-04-03 20:12:10 +0100

    Add Bio component

---
+++ src/components/Bio/index.js
@@ -0,0 +1,30 @@
+// @flow
+
+import React from 'react';
+import {
+  StyleSheet,
+  css,
+} from 'aphrodite/no-important';
+import linkify from 'util/linkify';
+
+type Props = {
+  text: string | null,
+};
+
+export default function Bio({text}: Props) {
+  if (!text) {return null;}
+  return (
+    <p
+      className={css(styles.Bio)}
+      dangerouslySetInnerHTML={{__html: linkify(text)}}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  Bio: {
+    fontSize: 18,
+    lineHeight: 1.3,
+  },
+});
+
