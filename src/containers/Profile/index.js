44b8633c6829a3bdd99303ed2f45d3fb6a8ad3ec
Simon Smith <simonsmith@users.noreply.github.com>
2017-04-03 20:30:11 +0100

    Add components to Profile

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -2,10 +2,18 @@ import React, {
   Component,
 } from 'react';
 import get from 'lodash/fp/get';
-// import {
-//   StyleSheet,
-// } from 'aphrodite/no-important';
+import {
+  css,
+  StyleSheet,
+} from 'aphrodite/no-important';
+import 'suitcss-utils-flex/lib/flex.css';
+import 'suitcss-utils-flex/lib/flex-sm.css';
 import Container from 'components/Container';
+import Avatar from 'components/Avatar';
+import Bio from 'components/Bio';
+import UserInfo from 'components/UserInfo';
+import ProfileTitle from 'components/ProfileTitle';
+import {viewport} from 'theme';
 import connect from './connect';
 
 type Props = {
@@ -38,8 +46,51 @@ export class ProfileContainer extends Component {
     if (this.props.userIsPending) {
       return <p>Loading...</p>;
     }
+
+    const {
+      avatar_url,
+      bio,
+      blog,
+      company,
+      html_url,
+      location,
+      login,
+      name,
+    } = this.props.userProfile;
+
     return (
-      <p>{this.props.userProfile.login}</p>
+      <div className={css(styles.Profile_container)}>
+        <div className="u-sm-flex">
+          <div className={css(styles.Profile_avatar)}>
+            <Avatar
+              name={name}
+              width={230}
+              url={avatar_url}
+            />
+          </div>
+          <div className="u-sm-flexGrow1">
+            <div className={css(styles.Profile_wrapTitle)}>
+              <ProfileTitle
+                name={name}
+                username={login}
+                userLink={html_url}
+              />
+            </div>
+            <div className={css(styles.Profile_wrapBio)}>
+              <Bio
+                text={bio}
+              />
+            </div>
+            <div className={css(styles.Profile_wrapUserInfo)}>
+              <UserInfo
+                location={location}
+                company={company}
+                blog={blog}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 
@@ -53,8 +104,27 @@ export class ProfileContainer extends Component {
 
 }
 
-// const styles = StyleSheet.create({
+const styles = StyleSheet.create({
+  Profile_container: {
+    paddingTop: 15,
+  },
 
-// });
+  Profile_wrapBio: {
+    marginBottom: 12,
+  },
+
+  Profile_avatar: {
+    marginBottom: 15,
+
+    [viewport.SM]: {
+      marginBottom: 0,
+      marginRight: 20,
+    },
+  },
+
+  Profile_wrapTitle: {
+    marginBottom: 5,
+  },
+});
 
 export default connect(ProfileContainer);
