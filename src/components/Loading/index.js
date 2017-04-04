292048d689761c7c83a3fcbe05a7479b83386d91
Simon Smith <simonsmith@users.noreply.github.com>
2017-04-04 22:46:45 +0100

    Remove styles from Loading component

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,23 +1,13 @@
 // @flow
 
 import React from 'react';
-import {
-  StyleSheet,
-  css,
-} from 'aphrodite/no-important';
 import 'suitcss-utils-flex/lib/flex.css';
 import LoadingSpinner from './loading.svg';
 
 export default function Loading() {
   return (
-    <div className={`${css(styles.Loading)} u-flex u-flexJustifyCenter u-flexAlignItemsCenter`}>
+    <div className="u-flex u-flexJustifyCenter u-flexAlignItemsCenter">
       <LoadingSpinner width={34} height={34} />
     </div>
   );
 }
-
-const styles = StyleSheet.create({
-  Loading: {
-    marginTop: 25,
-  },
-});
