5c2a7a8fa9e5d53cbbdd2e73460ff5dc3bbdf085
Simon Smith <simonsmith@users.noreply.github.com>
2017-04-23 19:35:46 +0100

    Add test for RateLimit connect

---
+++ test/containers/RateLimitConnect.spec.js
@@ -0,0 +1,27 @@
+import {
+  mapStateToProps,
+} from 'containers/RateLimit/connect';
+
+describe('RateLimitConnect: mapStateToProps', () => {
+
+  describe('when given the state', () => {
+    it('should return relevant properties', () => {
+      const state = {
+        rateLimit: {
+          core: {
+            limit: 3,
+            remaining: 5,
+            reset: 1492972397,
+          },
+          search: {
+            limit: 3,
+            remaining: 5,
+            reset: 1492972397,
+          },
+        },
+      };
+      expect(mapStateToProps(state)).toMatchSnapshot();
+    });
+  });
+
+});
